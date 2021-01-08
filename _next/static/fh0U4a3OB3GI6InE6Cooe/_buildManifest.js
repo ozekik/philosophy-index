407adbcfe390a32d4c00e953ed269273efcba27b
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2021-01-08 15:58:52 +0900

    Updates

---
+++ _next/static/fh0U4a3OB3GI6InE6Cooe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-2e2c5cb570ddbde1f93e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5057a88b95e4afa01d6b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
